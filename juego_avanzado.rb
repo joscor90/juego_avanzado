7fc3f5642658b8c3e8304376e533f54fe5614937
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-28 10:48:45 -0400

    Added validador method

--- juego_avanzado.rb
+++ juego_avanzado.rb
@@ -5,3 +5,17 @@ def menu(jugador)
         3. Tijera
         4. Salir"
 end
+
+def validador(opcion, jugador)
+    while opcion > 4 || opcion < 1 do
+        puts "Opción inválida por favor vuelve a elegir"
+        menu(jugador)
+        opcion = gets.chomp.to_i
+    end
+    return opcion
+end
+
+menu(1)
+opcion_1 = gets.chomp.to_i
+opcion_1 = validador(opcion_1, 1)
+
